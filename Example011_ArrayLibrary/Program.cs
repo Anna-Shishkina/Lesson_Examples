15c0e32d2cd600c3c355519d8c5765291a3609db
anna shishkina <ancho0002@gmail.com>
2023-01-17 23:53:23 +0300

    Add new Examples 008-014

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,54 @@
+﻿// Создай массив с 10 элементами и выведи на экран его значения от 1 до 10
+
+void FillArray(int[] collection) //заполни массив
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) //напиши массив
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;   //-1 если нет какого-то значения в массиве. 
+                         //Можно поставить "0", но тогда значение, которого нет в массиве программа определит как "0" и можно спутать что значение в индексе 0
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; //new int[10] - создай новый массив с 10ю элементами, т.е. поулчится массив с пустыми/нудевыми элементами 
+
+FillArray(array);
+// принудительно добавим еще четверок:
+array[4] = 4;
+array[6] = 6;
+
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 44); //попробуем найти значение 44 в массиве
+Console.WriteLine(pos);
---
+++ Example013_Text/Program.cs
@@ -0,0 +1,37 @@
+﻿// Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+// Ясна ли задача?
+
+string text = "- Я думаю, - Сказал князь, улыбаясь, - что, "
+            + "ежели бы вас послали виесто нашего миого Винценгероде,"
+            + "вы бы взяли приступо Согласие прусского короля. "
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = "qwerty"           
+//             012
+// s[3]  // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int lenght = text.Length;
+    for (int i = 0; i < lenght; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');   //заменить пробелы на "|"
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'к', 'К');   //заменить "к" на "К"
+Console.WriteLine(newText);
+Console.WriteLine();
+
+newText = Replace(newText, 'С', 'с');   //заменить "С" на "с"
+Console.WriteLine(newText);
